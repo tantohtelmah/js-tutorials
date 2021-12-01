400741f42e210dc358c856e9aecb79a60effac00
tantohtelmah <tantohtelmah@gmail.com>
2021-12-01 07:12:57 +0100

    journey with javascript

---
+++ jsStart.js
@@ -0,0 +1,8 @@
+document.write("hello i am starting javascript from fresh!<br>");
+var yourName = "Tantoh Telmah";
+var age = 19;
+document.getElementById("trial").innerHTML = "hello "+yourName +"<br> Age: "+age;
+var x = prompt("enter a number");
+document.write("2 * "+x+ " = "+2*x+"<br>");
+//convert string to numbers
+document.write("2 + number('3.4') = " , 2 + Number('3.4') )
